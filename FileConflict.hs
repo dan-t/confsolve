35e2d7a8fedf8342fcf4abd3e78aa671fbaf4b09
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-05-28 18:27:55 +0200

    Changed the application specific type class.
    Now the application specific code only parses the base name of the file
    path and returns the real base name - without the conflict info - and
    a text containing the details of the conflict.

--- FileConflict.hs
+++ FileConflict.hs
@@ -1,21 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module FileConflict where
-import System.FilePath
-import Data.Maybe
+import qualified Data.Text as T
+import qualified Filesystem.Path.CurrentOS as FP
+import Filesystem.Path.CurrentOS ((</>), (<.>))
 
--- the details and filePath of one conflicting file
+-- base name possibly containing conflict info
+type BaseName     = T.Text
+
+-- base name without conflict info
+type RealBaseName = T.Text
+
+-- conflicting file details displayed to the user
+type Details      = T.Text
+
+class ConflictParser a where
+   parseConflict :: a -> BaseName -> Maybe (RealBaseName, Details)
+
+-- the details and filePath of a conflicting file
+data ConflictingFile = ConflictingFile {
+   details  :: Details,
+   filePath :: FP.FilePath } deriving (Show)
+
+-- file path having a conflict
 data Conflict = Conflict {
-   details  :: String,
-   filePath :: FilePath } deriving (Show)
-
--- the info of all conflicting files
-data ConflictInfo = ConflictInfo {
-   dir       :: String,
-   fileName  :: String,
-   suffix    :: String,
-   conflicts :: [Conflict] } deriving (Show)
-
-origFilePath :: ConflictInfo -> FilePath
-origFilePath ci = dir ci </> fileName ci ++ suffix ci
-
-class FileConflict a where
-   find :: a -> FilePath -> IO (Maybe ConflictInfo)
+   origFilePath     :: FP.FilePath,
+   conflictingFiles :: [ConflictingFile]
+   } deriving (Show)
--- Wuala/Conflict.hs
+++ Wuala/Conflict.hs
@@ -1,44 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Wuala.Conflict where
-import Data.List
-import System.FilePath
 import FileConflict
-import Control.Applicative ((<$>))
-import qualified Wuala.FileNameParser as FNP
+import qualified Wuala.FileNameParser as P
 import qualified Data.Text as T
-import Utils
 
-data WualaConflict = WualaConflict
+data Parser = Parser
 
-instance FileConflict WualaConflict where
-   find fc file =
-      case FNP.parse (T.pack fileName) of
-           Just (FNP.FileInfo fn ver host) -> Just <$> conflictInfo dir (T.unpack fn) ext
-           Nothing                         -> return Nothing
-      where
-         (dir, fileName, ext) = splitFile file
+(<++>) = T.append
 
+instance ConflictParser Parser where
+   parseConflict _ baseName =
+      case P.parse baseName of
+           Just (P.FileInfo realBaseName version host) ->
+              Just (realBaseName, "Version " <++> version <++> " from " <++> host)
 
-splitFile file = (dir, fileName, ext)
-   where
-      (fileName, ext)        = splitExtensions fileNameWithExt
-      (dir, fileNameWithExt) = splitFileName file
-
-
-conflictInfo dir fileName suffix = do
-   conts <- getDirContents dir
-   return $ ConflictInfo dir fileName suffix (filterConfs conts [])
-   where
-      filterConfs []     confs = confs
-      filterConfs (c:cs) confs
-         | suffix /= ext = filterConfs cs confs
-         | otherwise =
-            case FNP.parse (T.pack fn) of
-                 Just (FNP.FileInfo f v h) ->
-                    if fileName == T.unpack f
-                       then let conf = Conflict ("Version " ++ T.unpack v ++ " from " ++ T.unpack h) (dir </> c)
-                                in filterConfs cs (conf : confs)
-                       else filterConfs cs confs
-
-                 Nothing -> filterConfs cs confs
-         where
-            (fn, ext) = splitExtensions c
+           Nothing -> Nothing
