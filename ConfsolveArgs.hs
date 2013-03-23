ac3b38f6c28becb765966a5042b3a59e77ac1f66
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2013-03-23 23:26:59 +0100

    confsolve 0.3.5

--- ConfsolveArgs.hs
+++ ConfsolveArgs.hs
@@ -19,7 +19,7 @@ confsolve = Confsolve {
    &= helpArg [explicit, name "help", name "h"]
    &= versionArg [explicit, name "version", name "v", summary versionInfo ]
 
-versionInfo = "confsolve version 0.3.4"
+versionInfo = "confsolve version 0.3.5"
 summaryInfo = ""
 
 confsolveArgs :: IO Confsolve
