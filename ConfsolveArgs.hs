8bcb246d648c15decc0e51de629cf6c00c6696eb
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2013-03-24 10:27:24 +0100

    Fix cabal package description

--- ConfsolveArgs.hs
+++ ConfsolveArgs.hs
@@ -19,7 +19,7 @@ confsolve = Confsolve {
    &= helpArg [explicit, name "help", name "h"]
    &= versionArg [explicit, name "version", name "v", summary versionInfo ]
 
-versionInfo = "confsolve version 0.3.6"
+versionInfo = "confsolve version 0.3.7"
 summaryInfo = ""
 
 confsolveArgs :: IO Confsolve
